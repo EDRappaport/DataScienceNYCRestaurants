3a90887de08c1a292c5da6be306c904f73f64c52
EDRappaport <ElliRappaport@gmail.com>
2015-05-05 23:20:54 -0400

    a few more graphs and by Cuisine

--- StudyYelpAndNYCData.R
+++ StudyYelpAndNYCData.R
@@ -44,7 +44,7 @@ aggregatedData <- transform(
   MaxScore = ave(SCORE, CAMIS, FUN = max),
   StdScore = ave(SCORE, CAMIS, FUN = sd))
 
-groupedData <- aggregate(SCORE ~ CAMIS+DBA+BORO+rating+review_count+VIOLATION_COUNT+MeanScore+MaxScore+MinScore+StdScore,
+groupedData <- aggregate(SCORE ~ CAMIS+DBA+BORO+CUISINE_DESCRIPTION+rating+review_count+VIOLATION_COUNT+MeanScore+MaxScore+MinScore+StdScore,
                          aggregatedData, FUN=mean)
 
 ggplot(data = dataWithoutNoneScoresAndGrades, aes(x=SCORE)) + geom_histogram()
@@ -58,8 +58,14 @@ ggplot(data=subset(dataWithoutNoneScoresAndGrades, grepl('BROOKLYN',BORO)),aes(x
 ggplot(data=groupedData,aes(x=rating,y=SCORE)) +  geom_point()
 
 attach(groupedData)
+xyplot(data$SCORE~as.factor(data$GRADE), xlab="Health Rating Letter Grade",
+       ylab="Health Rating Number Score", main="Health Inspection Scores by Grade")
+histogram(~data$SCORE|data$GRADE, xlab="Health Inspection Number Score",
+          main="Histogram of Health Inspection Number Score By Letter Grade")
 xyplot(review_count~MeanScore, xlab="Mean Health Rating Score (lower is better)",
        ylab="Number of Reviews", main="Number of Review on Yelp vs. NYC Health Score")
+xyplot(review_count~MeanScore|BORO, xlab="Mean Health Rating Score (lower is better)",
+       ylab="Number of Reviews", main="Number of Review on Yelp vs. NYC Health Score By Boro")
 xyplot(rating~MeanScore, xlab="Mean Health Rating Score (lower is better)",
        ylab="Average Yelp Rating", main="Average Yelp Review Rating vs. NYC Health Score")
 xyplot(VIOLATION_COUNT~rating, xlab="Average Yelp Rating",
@@ -71,8 +77,60 @@ dotplot(~rating|as.factor(BORO))
 histogram(MeanScore, breaks = seq(-2.5,65,2.5),
           type="count", xlab="Mean Health Rating Score (lower is better)",
           main="Histogram of the MeanScore")
+histogram(~MeanScore|BORO, breaks = seq(-2.5,65,2.5),
+          xlab="Mean Health Rating Score (lower is better)",
+          main="Histogram of the MeanScore By Boro")
+
+histogram(rating,
+          xlab = "Average Yelp Rating", main = "Histogram of Yelp Ratings")
+histogram(~rating|BORO,
+          xlab = "Average Yelp Rating", main = "Histogram of Yelp Ratings By Boro")
+histogram(review_count, breaks = c(seq(0,250,10), seq(251, 8000, 250)), xlim = c(0,250),
+          xlab="Number of Yelp Reviews", main="Histogram of Yelp Review Count")
+histogram(~review_count|BORO,
+          breaks = c(seq(0,250,10), seq(251, 8000, 250)), xlim = c(0,250),
+          xlab="Number of Yelp Reviews", main="Histogram of Yelp Review Count By Boro")
+
 xyplot(subset(groupedData, review_count > 25)$review_count~MeanScore,
        xlab="Mean Health Rating Score (lower is better)",
        ylab="Number of Yelp Reviews > 25 reviews",
        main="For Businesses with more than 25 Reviews: Number of Reviews vs Health Score")
+xyplot(subset(groupedData, review_count > 25)$review_count~MeanScore|BORO,
+       xlab="Mean Health Rating Score (lower is better)",
+       ylab="Number of Yelp Reviews > 25 reviews",
+       main="For Businesses with more than 25 Reviews: Number of Reviews vs Health Score By Boro")
 detach(groupedData)
+
+groupedDataForSomeCuisines <- subset(groupedData,
+                                     grepl('American', CUISINE_DESCRIPTION) |
+                                       grepl('Caribbean', CUISINE_DESCRIPTION) |
+                                       CUISINE_DESCRIPTION == 'Chinese' |
+                                       grepl('Italian', CUISINE_DESCRIPTION) |
+                                       grepl('Japanese', CUISINE_DESCRIPTION) |
+                                       grepl('Latin', CUISINE_DESCRIPTION) |
+                                       grepl('Mexican', CUISINE_DESCRIPTION) |
+                                       grepl('Pizza', CUISINE_DESCRIPTION) |
+                                       grepl('Jewish', CUISINE_DESCRIPTION) |
+                                       grepl('Soul Food', CUISINE_DESCRIPTION))
+
+attach(groupedDataForSomeCuisines)
+xyplot(review_count~MeanScore|CUISINE_DESCRIPTION,
+       xlab="Mean Health Rating Score (lower is better)",
+       ylab="Number of Reviews",
+       main="Number of Review on Yelp vs. NYC Health Score By Cuisine")
+xyplot(rating~MeanScore|CUISINE_DESCRIPTION,
+       xlab="Mean Health Rating Score (lower is better)",
+       ylab="Average Yelp Rating",
+       main="Average Yelp Review Rating vs. NYC Health Score By Cuisine")
+histogram(~MeanScore|CUISINE_DESCRIPTION, breaks = seq(-2.5,65,2.5),
+          xlab="Mean Health Rating Score (lower is better)",
+          main="Histogram of the MeanScore By Cuisine Type")
+
+histogram(~rating|CUISINE_DESCRIPTION,
+          xlab = "Average Yelp Rating", main = "Histogram of Yelp Ratings By Cuisine")
+histogram(~review_count|CUISINE_DESCRIPTION,
+          breaks = c(seq(0,250,10), seq(251, 8000, 250)), xlim = c(0,250),
+          xlab="Number of Yelp Reviews", main="Histogram of Yelp Review Count By Cuisine")
+detach(groupedDataForSomeCuisines)
+
+
